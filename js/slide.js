ab5afd8a4c75ef74895de4eb120cce75348e51cb
Mariana <marianadiscipula@gmail.com>
2022-08-08 18:22:51 -0300

    Next e Prev

--- js/slide.js
+++ js/slide.js
@@ -1,8 +1,12 @@
 export default class Slide {
     constructor(slide, wrapper) {
-        this.slide = document.querySelector(slide);
+        this.slide = document.querySelector(slide)
         this.wrapper = document.querySelector(wrapper);
         this.dist = { finalPosition: 0, startX: 0, movement: 0 }
+    }
+
+    transition(active) {
+        this.slide.style.transition = active ? 'transform .3s' : '';
     }
 
     moveSlide(distX) {
@@ -16,17 +20,17 @@ export default class Slide {
     }
 
     onStart(event) {
-        let moveType;
+        let movetype;
         if (event.type === 'mousedown') {
             event.preventDefault();
             this.dist.startX = event.clientX;
-            moveType = 'mousemove';
+            movetype = 'mousemove';
         } else {
             this.dist.startX = event.changedTouches[0].clientX;
-            moveType = 'touchmove';
+            movetype = 'touchmove';
         }
-        this.wrapper.addEventListener(moveType, this.onMove);
-
+        this.wrapper.addEventListener(movetype, this.onMove);
+        this.transition(false);
     }
 
     onMove(event) {
@@ -36,31 +40,48 @@ export default class Slide {
     }
 
     onEnd(event) {
-        const moveType = (event.type === 'mouseup') ? 'mousemove' : 'touchmove';
-        this.wrapper.removeEventListener(moveType, this.onMove);
+        const movetype = (event.type === 'mouseup') ? 'mousemove' : 'touchmove';
+        this.wrapper.removeEventListener(movetype, this.onMove);
         this.dist.finalPosition = this.dist.movePosition;
+        this.transition(true);
+        this.changeSlideOnEnd();
+    }
+
+    changeSlideOnEnd() {
+        if (this.dist.movement > 120 && this.index.next !== undefined) {
+            this.activeNextSlide();
+        } else if (this.dist.movement < -120 && this.index.prev !== undefined) {
+            this.activePrevSlide();
+        } else {
+            this.changeSlide(this.index.active);
+        }
     }
 
     addSlideEvents() {
         this.wrapper.addEventListener('mousedown', this.onStart);
-        this.wrapper.addEventListener('mouseup', this.onEnd);
         this.wrapper.addEventListener('touchstart', this.onStart);
+        this.wrapper.addEventListener('mouseup', this.onEnd);
         this.wrapper.addEventListener('touchend', this.onEnd);
     }
 
-    slidePosition() {
-        const margin = (this.wrapper.offsetWidth - this.slide.offsetWidth) / 2;
-        return -(this.slide.offsetLeft - margin);
+    bindEvents() {
+        this.onStart = this.onStart.bind(this);
+        this.onMove = this.onMove.bind(this);
+        this.onEnd = this.onEnd.bind(this);
+    }
+
+    // Slides config
+
+    slidePosition(slide) {
+        const margin = (this.wrapper.offsetWidth - slide.offsetWidth) / 2;
+        return -(slide.offsetLeft - margin);
     }
 
     slidesConfig() {
-        this.slideArray = [...this.slide.children].map(ele => {
-            const position = this.slidePosition(ele);
-            return {
-                position,
-                ele
-            }
-        })
+        this.slideArray = [...this.slide.children].map((element) => {
+            const position = this.slidePosition(element);
+            return { position, element };
+        });
     }
 
     slidesIndexNav(index) {
@@ -79,14 +100,17 @@ export default class Slide {
         this.dist.finalPosition = activeSlide.position;
     }
 
-    bindEvents() {
-        this.onStart = this.onStart.bind(this);
-        this.onMove = this.onMove.bind(this);
-        this.onEnd = this.onEnd.bind(this);
+    activePrevSlide() {
+        if (this.index.prev !== undefined) this.changeSlide(this.index.prev);
+    }
+
+    activeNextSlide() {
+        if (this.index.next !== undefined) this.changeSlide(this.index.next);
     }
 
     init() {
         this.bindEvents();
+        this.transition(true);
         this.addSlideEvents();
         this.slidesConfig();
         return this;
